b7f6518537bd78b8f4c8f1e2fc3b03df1ea60339
Trocha4 <trocha@fi.uba.ar>
2025-05-12 17:58:05 +0000

    Boceto Lobby

---
+++ src/client/lobby/lobby.h
@@ -0,0 +1,40 @@
+#ifndef LOBBY_H
+#define LOBBY_H
+
+#include <QMainWindow>
+#include <vector>
+#include <string>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Lobby;
+}
+QT_END_NAMESPACE
+
+class Lobby : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Lobby(QWidget *parent = nullptr);
+    ~Lobby();
+
+private slots:
+    void on_CreateGame_clicked();
+
+    void on_JoinGame_clicked();
+
+    void go_to_lobby();
+
+    void on_JoinGameButton_clicked();
+
+    void on_createButton_clicked();
+
+    void connect_to_sv();
+
+private:
+    Ui::Lobby *ui;
+    std::vector<std::string> partidas;
+    //Protocolo
+};
+#endif // LOBBY_H
