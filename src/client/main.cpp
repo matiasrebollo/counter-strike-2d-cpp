231f7ef120e604dcf75f9db43e3eb1ca51f2d0b9
matiasrebollo <mrebollo@fi.uba.ar>
2025-05-12 03:37:16 -0300

    El jugador mira el mouse

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,44 +1,50 @@
 #include <exception>
 #include <iostream>
+#include <cmath>
 
 #include <SDL2/SDL.h>
 #include <SDL2pp/SDL2pp.hh>
 
-#include "common/foo.h"
-
 using namespace SDL2pp;
 
-#define DATA_PATH "/home/matias/Escritorio/TALLER 1/TP-GRUPAL/cs-2d-grupo-5-2025c1/assets/gfx"
+#define DATA_PATH "/home/matias/Escritorio/TALLER 1/TP-GRUPAL/cs-2d-grupo-5-2025c1/assets/gfx" //deberia cambiarlo
 
 int main() try {
-    // Initialize SDL library
+    /* Inicializamos sdl */
     SDL sdl(SDL_INIT_VIDEO);
 
-    // Create main window: 640x480 dimensions, resizable, "SDL2pp demo" title
+    /* Creamos la ventana de 640x480 (puede cambiar) y posicionada automaticamente */
     Window window("SDL2pp demo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480,
                   SDL_WINDOW_RESIZABLE);
 
-    // Create accelerated video renderer with default driver
+    /* Creamos el renderer que se encarga de dibujar en window */
     Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     Surface spriteSheet(DATA_PATH "/player/ct1.bmp");
     Texture sprite(renderer, spriteSheet);
 
+    /* Comenzamos en el centro */
     float x_pos = (renderer.GetOutputWidth() - 32) / 2;
     float y_pos = (renderer.GetOutputHeight() - 32) / 2;
 
+    /* Booleanos para ver que tecla se toca */
     bool w = false, a = false, s = false, d = false;
 
+    /* Nos guardamos el tiempo en ms desde que inicializamos sdl */
     unsigned int prev_ticks = SDL_GetTicks();
     while (true) {
+        /* Nos guardamos la diferencia de ms entre este frame y el anterior */
         unsigned int frame_ticks = SDL_GetTicks();
         unsigned int frame_delta = frame_ticks - prev_ticks;
         prev_ticks = frame_ticks;
 
+        /* Procesamos el evento */
         SDL_Event event;
         while (SDL_PollEvent(&event)){
+            /* Si se cierra la ventana salimos del juego */
             if (event.type == SDL_QUIT){
                 return 0;
+            /* Si se toca una tecla activamos su booleano o si es Esc cerramos el juego */
             } else if (event.type == SDL_KEYDOWN) {
                 switch (event.key.keysym.sym) {
                     case SDLK_ESCAPE: return 0;
@@ -47,6 +53,7 @@ int main() try {
                     case SDLK_s: s = true; break;
                     case SDLK_d: d = true; break;
                 }
+            /* Si se suelta una tecla desactivamos su booleano */
             } else if (event.type == SDL_KEYUP) {
                 switch(event.key.keysym.sym) {
                     case SDLK_w: w = false; break;
@@ -57,7 +64,10 @@ int main() try {
             } 
         }
 
+        /* Determina cuanto se mueve el personaje en este frame basado en cuanto tiempo paso */
         float movimiento = frame_delta * 0.2f;
+
+        /* Actualizamos posicion segun corresponda sin pasarnos de los bordes */
         if (w && y_pos > 0){
             y_pos -= movimiento;
         }
@@ -71,12 +81,24 @@ int main() try {
             x_pos += movimiento;
         }
 
+        /* Obtenemos posicion del mouse */
+        int mouse_x, mouse_y;
+        SDL_GetMouseState(&mouse_x, &mouse_y);
+
+        /* Calculamos angulo entre el personaje y el cursor */
+        float dx = mouse_x - x_pos;
+        float dy = mouse_y - y_pos;
+        float ang_radianes = atan2(dy, dx); //angulo en radianes
+        double angulo = (ang_radianes * 180.0f / M_PI) + 90; //conversion de radianes a grados
+
         // Clear screen
         renderer.Clear();
 
         renderer.Copy(sprite,
                   Rect(0, 32, 32, 32),
-                  Rect((int)x_pos, (int)y_pos, 32, 32));
+                  Rect((int)x_pos, (int)y_pos, 32, 32),
+                  angulo, 
+                  Point(16.0f, 16.0f)); // para que rote al rededor del centro del sprite y no de su izq sup
 
         // Show rendered frame
         renderer.Present();
