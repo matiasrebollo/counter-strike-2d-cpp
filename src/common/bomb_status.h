4e74d065ef23222f0391663606d2a6ed79ce1341
Taiel Molina <tamolina@fi.uba.ar>
2025-05-22 11:48:53 -0300

    Revert "Fixing protocol (and more things)"

--- src/common/bomb_status.h
+++
@@ -1,6 +0,0 @@
-#ifndef BOMB_STATUS_H
-#define BOMB_STATUS_H
-
-enum BombStatus { NOT_PLANTED, PLANTED, EXPLODE };
-
-#endif
--- src/common/phase.h
+++
@@ -1,6 +0,0 @@
-#ifndef PHASE_H
-#define PHASE_H
-
-enum Phase { WAITING_PLAYERS, BUY, ATTACK, ENDED };
-
-#endif
