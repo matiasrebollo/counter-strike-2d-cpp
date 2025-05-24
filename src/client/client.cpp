241ec23709392c59ce9e70191d618673843058b2
matiasrebollo <mrebollo@fi.uba.ar>
2025-05-24 16:22:58 -0300

    Funciona rotacion y movimiento bugeado

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -47,38 +47,82 @@ void Client::run(int argc, char* argv[]) {
     SDL2pp::Surface boxSheet("../assets/gfx/tiles/aztec.bmp");
     SDL2pp::Texture box(renderer, boxSheet);
 
-    float x_pos = 100;
-    float y_pos = 100;
+    Snapshot last_snapshot;
+    receiver.try_pop_snapshot_from_queue(last_snapshot);
+    const PlayerDTO& p_inicial = last_snapshot.players[0];
+    float x_pos = p_inicial.position.x;
+    float y_pos = p_inicial.position.y;
+
+    bool w = false, a = false, s = false, d = false;
+
 
     int it = 0;
     int FPS = 30;
     Clock clock;
-    Snapshot last_snapshot;
-    receiver.try_pop_snapshot_from_queue(last_snapshot);
     while (true) {
-        auto frame_start = std::chrono::steady_clock::now();  // INICIO DEL FRAME
+        // auto frame_start = std::chrono::steady_clock::now();  // INICIO DEL FRAME
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT)
                 return;
-            /*if (event.type == SDL_KEYDOWN) {
+            if (event.type == SDL_KEYDOWN) {
                 switch (event.key.keysym.sym) {
                     case SDLK_ESCAPE:
                         return;
                     case SDLK_w:
-                        sender.add_command_to_queue(MoveUpDTO{});
+                        if (!w) {
+                            sender.add_command_to_queue(MoveUpDTO{});
+                            w = true;
+                        }
                         break;
                     case SDLK_a:
-                        sender.add_command_to_queue(MoveLeftDTO{});
+                        if (!a) {
+                            sender.add_command_to_queue(MoveLeftDTO{});
+                            a = true;
+                        }
                         break;
                     case SDLK_s:
-                        sender.add_command_to_queue(MoveDownDTO{});
+                        if (!s) {
+                            sender.add_command_to_queue(MoveDownDTO{});
+                            s = true;
+                        }
                         break;
                     case SDLK_d:
-                        sender.add_command_to_queue(MoveRightDTO{});
+                        if (!d) {
+                            sender.add_command_to_queue(MoveRightDTO{});
+                            d = true;
+                        }
                         break;
                 }
-            }*/
+            }
+            if (event.type == SDL_KEYUP) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_w:
+                        if (w) {
+                            sender.add_command_to_queue(MoveUpDTO{});
+                            w = false;
+                        }
+                        break;
+                    case SDLK_a:
+                        if (a) {
+                            sender.add_command_to_queue(MoveLeftDTO{});
+                            a = false;
+                        }
+                        break;
+                    case SDLK_s:
+                        if (s) {
+                            sender.add_command_to_queue(MoveDownDTO{});
+                            s = false;
+                        }
+                        break;
+                    case SDLK_d:
+                        if (d) {
+                            sender.add_command_to_queue(MoveRightDTO{});
+                            d = false;
+                        }
+                        break;
+                }
+            }
             if (event.type == SDL_MOUSEMOTION) {
                 int mouse_x = event.motion.x;
                 int mouse_y = event.motion.y;
@@ -87,70 +131,79 @@ void Client::run(int argc, char* argv[]) {
                 float ang_radianes = atan2(dy, dx);
                 const double angulo = (ang_radianes * 180.0f / M_PI) + 90;
 
-                auto send_start = std::chrono::steady_clock::now();
+                // auto send_start = std::chrono::steady_clock::now();
                 sender.add_command_to_queue(RotateDTO{angulo});
-                auto send_end = std::chrono::steady_clock::now();
-                std::cout << "[TIMER] Envío comando: "
+                // auto send_end = std::chrono::steady_clock::now();
+                /*std::cout << "[TIMER] Envío comando: "
                           << std::chrono::duration_cast<std::chrono::microseconds>(send_end -
                                                                                    send_start)
                                      .count()
-                          << " us\n";
+                          << " us\n";*/
             }
         }
 
-        auto pop_start = std::chrono::steady_clock::now();
-        int pop_count = 0;
+        // auto pop_start = std::chrono::steady_clock::now();
+        // int pop_count = 0;
         Snapshot snapshot_tmp;
         while (receiver.try_pop_snapshot_from_queue(snapshot_tmp)) {
             last_snapshot = std::move(snapshot_tmp);
-            pop_count++;
+            // pop_count++;
         }
-        auto pop_end = std::chrono::steady_clock::now();
-        std::cout << "[TIMER] try_pop_snapshot: "
+        // auto pop_end = std::chrono::steady_clock::now();
+        /*std::cout << "[TIMER] try_pop_snapshot: "
                   << std::chrono::duration_cast<std::chrono::microseconds>(pop_end - pop_start)
                              .count()
-                  << " us | Snapshots nuevos: " << pop_count << "\n";
+                  << " us | Snapshots nuevos: " << pop_count << "\n";*/
 
         const PlayerDTO& p = last_snapshot.players[0];
         double angulo = p.orientation;
+        x_pos = p.position.x;
+        y_pos = p.position.y;
 
-        auto render_start = std::chrono::steady_clock::now();
+        std::cout << "Posicion x: " << x_pos << std::endl;
+        std::cout << "Posicion y: " << y_pos << std::endl;
+        std::cout << "\n";
+
+        // auto render_start = std::chrono::steady_clock::now();
         renderer.Clear();
 
         // ACA SI ITERO EL MAPA (POR AHORA SOLO TIPO BOX)
         for (const MapObject& obj: map.map_objects) {
             if (obj.type == MapObjectType::BOX) {
                 SDL2pp::Rect rect_origen(416, 64, 32, 32);  // por ahora lo hardcodeo
+                std::cout << "Posicion caja x: " << obj.position.x << std::endl;
+                std::cout << "Posicion caja y: " << obj.position.y << std::endl;
+                std::cout << "Caja ancho: " << obj.width << std::endl;
+                std::cout << "Caja alto: " << obj.height << std::endl;
                 SDL2pp::Rect rect_destino(obj.position.x, obj.position.y, obj.width, obj.height);
                 renderer.Copy(box, rect_origen, rect_destino);
             }
         }
 
         // podria tambien crear los rect y point antes en lugar de en el copy
-        renderer.Copy(player, SDL2pp::Rect(0, 32, 32, 32),
-                      SDL2pp::Rect(p.position.x, p.position.y, 32, 32), angulo,
-                      SDL2pp::Point(16.0f, 16.0f));
+        renderer.Copy(player, SDL2pp::Rect(0, 32, 32, 32), SDL2pp::Rect(x_pos, y_pos, 32, 32),
+                      angulo, SDL2pp::Point(16.0f, 16.0f));
         renderer.Present();
 
-        auto render_end = std::chrono::steady_clock::now();
-        std::cout << "[TIMER] Render: "
+        // auto render_end = std::chrono::steady_clock::now();
+        /*std::cout << "[TIMER] Render: "
                   << std::chrono::duration_cast<std::chrono::microseconds>(render_end -
                                                                            render_start)
                              .count()
-                  << " us\n";
+                  << " us\n";*/
 
-        auto sleep_start = std::chrono::steady_clock::now();
+        // auto sleep_start = std::chrono::steady_clock::now();
         it = clock.sleep_and_calc_next_it(FPS, it);
-        auto sleep_end = std::chrono::steady_clock::now();
-        std::cout << "[TIMER] Sleep: "
+        // auto sleep_end = std::chrono::steady_clock::now();
+        /*std::cout << "[TIMER] Sleep: "
                   << std::chrono::duration_cast<std::chrono::microseconds>(sleep_end - sleep_start)
                              .count()
-                  << " us\n";
+                  << " us\n";*/
 
-        auto frame_end = std::chrono::steady_clock::now();
-        std::cout << "[TIMER] Frame completo: "
+        // auto frame_end = std::chrono::steady_clock::now();
+        /*std::cout << "[TIMER] Frame completo: "
                   << std::chrono::duration_cast<std::chrono::milliseconds>(frame_end - frame_start)
                              .count()
-                  << " ms\n\n";
+                  << " ms\n\n";*/
     }
 }
