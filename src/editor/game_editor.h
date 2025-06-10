08489f252f9834fc7da42a8aefb7e656b30ea529
Trocha4 <trocha@fi.uba.ar>
2025-06-10 13:51:22 +0000

    editor, agrego sites y spawn

--- src/editor/game_editor.h
+++ src/editor/game_editor.h
@@ -11,8 +11,10 @@
 #include "../common/game_map.h"
 
 #include "blocks_setter.h"
+#include "bomb_sites_setter.h"
 #include "ct_spawns_setter.h"
 #include "grid_action.h"
+#include "tt_spawns_setter.h"
 
 // cppcheck-suppress unknownMacro
 QT_BEGIN_NAMESPACE
@@ -27,8 +29,11 @@ class Game_editor: public QMainWindow {
 public:
     explicit Game_editor(QWidget* parent = nullptr);
     ~Game_editor();
+    void setupToolbar();
     void setBlock(const int& row, const int& colum);
     void setCtSpawn(const int& row, const int& colum);
+    void setTTSpawn(const int& row, const int& colum);
+    void setBombSite(const int& row, const int& colum);
 
 private slots:
     void on_save_button_clicked();
@@ -44,14 +49,16 @@ private:
     void setupBlockList();
     void setupBackgroundList();
     void setupGridMap(const int& rows, const int& colums);
-
+    std::vector<Vector2D<int>> set_to_vector(const std::set<std::pair<int, int>>& set_pos);
 
     BlockTextureParser texture_parser;
     int selected_block;
     Background selected_background;
     std::vector<std::vector<int>> grid;
     std::unique_ptr<GridAction> mode;
+    std::set<std::pair<int, int>> tt_spawns;
     std::set<std::pair<int, int>> ct_spawns;
+    std::set<std::pair<int, int>> bomb_sites;
     std::pair<int, int> first_click;
     std::pair<int, int> second_click;
     bool first_click_done;
