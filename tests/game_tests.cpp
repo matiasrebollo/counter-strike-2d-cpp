36cfb9c79ebc926832886291a88ba3b06e15f0f7
Taielmolina01 <tamolina@fi.uba.ar>
2025-06-16 15:09:52 -0300

    fixing tests

--- tests/game_tests.cpp
+++ tests/game_tests.cpp
@@ -43,8 +43,7 @@ TEST(ClientProtocolTest, SendRotate) {
         CommandDTO request = server->receive_client_request();
         auto RotateDTOPtr = std::get_if<RotateDTO>(&request);
         ASSERT_NE(RotateDTOPtr, nullptr) << "Expected RotateDTO but got another";
-        ASSERT_GE(RotateDTOPtr->angle, angle - 0.01);
-        ASSERT_LE(RotateDTOPtr->angle, angle + 0.01);
+        ASSERT_NEAR(RotateDTOPtr->angle, angle, 0.1);
     }
 }
 
@@ -251,8 +250,7 @@ void validate_player(const PlayerDTO& expected_player, const PlayerDTO& actual_p
     ASSERT_EQ(expected_player.loadout.equipped, actual_player.loadout.equipped);
     ASSERT_EQ(expected_player.shot.has_value(), actual_player.shot.has_value());
     if (expected_player.shot.has_value()) {
-        ASSERT_LE(expected_player.shot->distance, actual_player.shot->distance - 0.1);
-        ASSERT_GE(expected_player.shot->distance, actual_player.shot->distance + 0.1);
+        ASSERT_NEAR(expected_player.shot->distance, actual_player.shot->distance, 0.1);
     }
 }
 
